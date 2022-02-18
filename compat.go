5d4f404a69598ce74f3a03d55630d3f5d9987b67
Christian Banse <oxisto@aybaze.com>
2022-02-18 12:56:38 +0100

    Added more compatibility with the `golang.org/x/oauth2` package

--- compat.go
+++ compat.go
@@ -2,6 +2,26 @@ package oauth2
 
 import "golang.org/x/oauth2"
 
-type Config = oauth2.Config
+// AuthCodeOption is a type alias for https://pkg.go.dev/golang.org/x/oauth2#AuthCodeOption.
+type AuthCodeOption = oauth2.AuthCodeOption
+
+// AuthStyle is a type alias for https://pkg.go.dev/golang.org/x/oauth2#AuthStyle.
 type AuthStyle = oauth2.AuthStyle
+
+// Config is a type alias for https://pkg.go.dev/golang.org/x/oauth2#Config.
+type Config = oauth2.Config
+
+// Endpoint is a type alias for https://pkg.go.dev/golang.org/x/oauth2#Endpoint.
 type Endpoint = oauth2.Endpoint
+
+// RetrieveError is a type alias for https://pkg.go.dev/golang.org/x/oauth2#RetrieveError.
+type RetrieveError = oauth2.RetrieveError
+
+// Token is a type alias for https://pkg.go.dev/golang.org/x/oauth2#Token.
+type Token = oauth2.Token
+
+// TokenSource is a type alias for https://pkg.go.dev/golang.org/x/oauth2#TokenSource.
+type TokenSource = oauth2.TokenSource
+
+// Transport is a type alias for https://pkg.go.dev/golang.org/x/oauth2#Transport.
+type Transport = oauth2.Transport
